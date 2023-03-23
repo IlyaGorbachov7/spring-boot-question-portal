b1ee4653a1f6a90d95c9dbc5b56755df3c5d4527
Ilyz <gorbacevaili891@gmail.com>
2023-03-24 01:44:59 +0300

    Initial commit

---
+++ src/main/java/softarex/gorbachev/springbootquestionportal/SpringBootQuestionPortalApplication.java
@@ -0,0 +1,13 @@
+package softarex.gorbachev.springbootquestionportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootQuestionPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootQuestionPortalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/softarex/gorbachev/springbootquestionportal/SpringBootQuestionPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package softarex.gorbachev.springbootquestionportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootQuestionPortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
