b4a060c967009e35143dc047a5e039adf3e35628
Ilyz <gorbacevaili891@gmail.com>
2023-03-31 14:14:34 +0300

    @Trannsactional -> readOnly = true

--- src/main/java/softarex/gorbachev/springbootquestionportal/controller/AppRestExceptionHandler.java
+++ src/main/java/softarex/gorbachev/springbootquestionportal/controller/AppRestExceptionHandler.java
@@ -7,23 +7,24 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import softarex.gorbachev.springbootquestionportal.exception.login.LoginException;
 import softarex.gorbachev.springbootquestionportal.exception.QuestionPortalServerException;
+import softarex.gorbachev.springbootquestionportal.model.BadMessageResponse;
 import softarex.gorbachev.springbootquestionportal.model.MessageResponse;
 
 @RestControllerAdvice
 public class AppRestExceptionHandler {
 
     @ExceptionHandler({QuestionPortalServerException.class})
-    public ResponseEntity<MessageResponse> handlerGeneralExceptions(QuestionPortalServerException ex) {
-        return ResponseEntity.badRequest().body(new MessageResponse(ex.getMessage()));
+    public ResponseEntity<BadMessageResponse> handlerGeneralExceptions(QuestionPortalServerException ex) {
+        return ResponseEntity.badRequest().body(new BadMessageResponse(ex.getMessage()));
     }
 
     @ExceptionHandler({UsernameNotFoundException.class})
-    public ResponseEntity<MessageResponse> handlerJWTTokenExceptions(UsernameNotFoundException ex) {
-        return ResponseEntity.status(HttpStatus.NON_AUTHORITATIVE_INFORMATION).body(new MessageResponse("JWT token is miss: " + ex.getMessage()));
+    public ResponseEntity<BadMessageResponse> handlerJWTTokenExceptions(UsernameNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NON_AUTHORITATIVE_INFORMATION).body(new BadMessageResponse("Sorry brooo! JWT token is missed: " + ex.getMessage()));
     }
 
     @ExceptionHandler(LoginException.class)
-    public ResponseEntity<MessageResponse> handlerLoginExceptions(LoginException ex) {
-        return ResponseEntity.badRequest().body(new MessageResponse(ex.getMessage()));
+    public ResponseEntity<BadMessageResponse> handlerLoginExceptions(LoginException ex) {
+        return ResponseEntity.badRequest().body(new BadMessageResponse(ex.getMessage()));
     }
 }
---
+++ src/main/java/softarex/gorbachev/springbootquestionportal/model/BadMessageResponse.java
@@ -0,0 +1,10 @@
+package softarex.gorbachev.springbootquestionportal.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class BadMessageResponse {
+    private String error;
+}
